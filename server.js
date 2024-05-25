5b6ca88244c37fca44b4e5d21b41b2ddf3dbdbdf
Nandu-Ramineni <nandurameneni5577@gmail.com>
2024-05-25 12:42:20 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,87 @@
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 8000;
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@file.hng0fkz.mongodb.net/?retryWrites=true&w=majority&appName=file`);
+const conn = mongoose.connection;
+
+conn.once('open', () => {
+    console.log('Connected to MongoDB Atlas');
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+const fileSchema = new mongoose.Schema({
+    filename: String,
+    contentType: String,
+    uploadDate: Date,
+});
+const File = mongoose.model('File', fileSchema);
+
+app.post('/upload', upload.single('file'), (req, res) => {
+
+    if (req.file) {
+        const file = new File({
+            filename: req.file.originalname,
+            contentType: req.file.mimetype,
+            uploadDate: new Date()
+        });
+        file.save().then((savedFile) => {
+            const fileData = {
+                id: savedFile._id,
+                filename: savedFile.filename,
+                contentType: savedFile.contentType,
+                uploadDate: savedFile.uploadDate
+            };
+            if (req.body.lat && req.body.lon) {
+                res.json({
+                    message: 'File uploaded successfully with location data.',
+                    file: fileData,
+                    location: {
+                        lat: req.body.lat,
+                        lon: req.body.lon
+                    }
+                });
+            } else {
+                res.json({
+                    message: 'File uploaded successfully.',
+                    file: fileData
+                });
+            }
+        }).catch((error) => {
+            console.error('Error uploading file:', error);
+            res.status(500).send('Error uploading file.');
+        });
+    } else {
+        if (req.body.lat && req.body.lon) {
+            res.json({
+                message: 'Location data received.',
+                location: {
+                    lat: req.body.lat,
+                    lon: req.body.lon
+                }
+            });
+        } else {
+            res.status(400).send('No file or location data uploaded.');
+        }
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}
+);
